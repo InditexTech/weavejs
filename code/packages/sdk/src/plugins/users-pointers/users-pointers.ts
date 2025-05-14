e60c5120ac5bf7806492beaba0fdcbb6bbb74fbc
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-05-14 12:24:09 +0200

    feat: improved create-app frontend UI

--- code/packages/sdk/src/plugins/users-pointers/users-pointers.ts
+++ code/packages/sdk/src/plugins/users-pointers/users-pointers.ts
@@ -24,9 +24,7 @@ export class WeaveUsersPointersPlugin extends WeavePlugin {
     string,
     { oldPos: WeaveUserPointer; actualPos: WeaveUserPointer }
   >;
-  // private usersPointersTimers: Record<string, NodeJS.Timeout>;
   private config!: WeaveUsersPointersPluginConfig;
-  private doRenderPointers: boolean;
   private userPointerCircleRadius: number = 4;
   private userPointerSeparation: number = 8;
   private userPointerCircleStrokeWidth: number = 1;
@@ -40,9 +38,7 @@ export class WeaveUsersPointersPlugin extends WeavePlugin {
 
     const { config } = params;
 
-    this.doRenderPointers = true;
     this.usersPointers = {};
-    // this.usersPointersTimers = {};
     this.config = config;
   }
 
@@ -196,152 +192,136 @@ export class WeaveUsersPointersPlugin extends WeavePlugin {
       return;
     }
 
-    if (this.doRenderPointers) {
-      for (const userPointerKey of Object.keys(this.usersPointers)) {
-        const userPointer = this.usersPointers[userPointerKey];
+    for (const userPointerKey of Object.keys(this.usersPointers)) {
+      const userPointer = this.usersPointers[userPointerKey];
 
-        const userPointerNode = pointersLayer?.findOne(
-          `#${userPointer.actualPos.user}`
-        ) as Konva.Group | undefined;
+      const userPointerNode = pointersLayer?.findOne(
+        `#${userPointer.actualPos.user}`
+      ) as Konva.Group | undefined;
 
-        if (!userPointerNode) {
-          const userPointerNode = new Konva.Group({
-            name: 'pointer',
-            id: userPointer.actualPos.user,
-            x: userPointer.actualPos.x,
-            y: userPointer.actualPos.y,
-            opacity: 1,
-            listening: false,
-          });
-
-          const userPointNode = new Konva.Circle({
-            id: 'userPoint',
-            x: 0,
-            y: 0,
-            radius: this.userPointerCircleRadius / stage.scaleX(),
-            fill: this.stringToColour(userPointer.actualPos.user),
-            stroke: 'black',
-            strokeWidth: this.userPointerCircleStrokeWidth / stage.scaleX(),
-            listening: false,
-          });
-
-          const userNameNode = new Konva.Text({
-            id: 'userPointName',
-            x:
-              (this.userPointerSeparation +
-                this.userPointerBackgroundPaddingX) /
-              stage.scaleX(),
-            y: 0 / stage.scaleX(),
-            height: (userPointNode.height() * 2) / stage.scaleX(),
-            text: userPointer.actualPos.user,
-            fontSize: this.userPointerNameFontSize / stage.scaleX(),
-            fontFamily: 'NotoSansMono, monospace',
-            fill: 'black',
-            align: 'left',
-            verticalAlign: 'middle',
-            listening: false,
-          });
-
-          const userNameBackground = new Konva.Rect({
-            id: 'userPointRect',
-            x: this.userPointerSeparation / stage.scaleX(),
-            y: -this.userPointerBackgroundPaddingY / stage.scaleX(),
-            width:
-              (userNameNode.width() + this.userPointerBackgroundPaddingX * 2) /
-              stage.scaleX(),
-            height:
-              (userNameNode.height() + this.userPointerBackgroundPaddingY * 2) /
-              stage.scaleX(),
-            cornerRadius:
-              this.userPointerBackgroundCornerRadius / stage.scaleX(),
-            fill: 'rgba(0,0,0,0.2)',
-            strokeWidth: 0,
-            listening: false,
-          });
-
-          userPointNode.setAttrs({
-            y: userNameBackground.y() + userNameBackground.height() / 2,
-          });
-
-          userPointerNode.add(userPointNode);
-          userPointerNode.add(userNameBackground);
-          userPointerNode.add(userNameNode);
-
-          pointersLayer?.add(userPointerNode);
-
-          continue;
-        }
-
-        const oldPos: Vector2d = {
-          x: userPointer.oldPos.x,
-          y: userPointer.oldPos.y,
-        };
-        const actualPos: Vector2d = {
+      if (!userPointerNode) {
+        const userPointerNode = new Konva.Group({
+          name: 'pointer',
+          id: userPointer.actualPos.user,
           x: userPointer.actualPos.x,
           y: userPointer.actualPos.y,
-        };
-        const hasChanged = !_.isEqual(actualPos, oldPos);
-
-        // UPDATE TO SCALE
-        const userPointNode = userPointerNode.getChildren(
-          (node) => node.getAttrs().id === 'userPoint'
-        );
-        userPointNode[0]?.setAttrs({
-          radius: this.userPointerCircleRadius / stage.scaleX(),
-          strokeWidth: this.userPointerCircleStrokeWidth / stage.scaleX(),
+          opacity: 1,
+          listening: false,
         });
-        const userPointNodeText = userPointerNode.getChildren(
-          (node) => node.getAttrs().id === 'userPointName'
-        );
-        userPointNodeText[0]?.setAttrs({
+
+        const userPointNode = new Konva.Circle({
+          id: 'userPoint',
+          x: 0,
+          y: 0,
+          radius: this.userPointerCircleRadius / stage.scaleX(),
+          fill: this.stringToColour(userPointer.actualPos.user),
+          stroke: 'black',
+          strokeWidth: this.userPointerCircleStrokeWidth / stage.scaleX(),
+          listening: false,
+        });
+
+        const userNameNode = new Konva.Text({
+          id: 'userPointName',
           x:
             (this.userPointerSeparation + this.userPointerBackgroundPaddingX) /
             stage.scaleX(),
           y: 0 / stage.scaleX(),
-          height: userPointNode[0]?.height(),
+          height: (userPointNode.height() * 2) / stage.scaleX(),
+          text: userPointer.actualPos.user,
           fontSize: this.userPointerNameFontSize / stage.scaleX(),
+          fontFamily: 'NotoSansMono, monospace',
+          fill: 'black',
+          align: 'left',
+          verticalAlign: 'middle',
+          listening: false,
         });
-        const userPointNodeBackground = userPointerNode.getChildren(
-          (node) => node.getAttrs().id === 'userPointRect'
-        );
-        userPointNodeBackground[0]?.setAttrs({
+
+        const userNameBackground = new Konva.Rect({
+          id: 'userPointRect',
           x: this.userPointerSeparation / stage.scaleX(),
           y: -this.userPointerBackgroundPaddingY / stage.scaleX(),
-          cornerRadius: this.userPointerBackgroundCornerRadius / stage.scaleX(),
           width:
-            userPointNodeText[0]?.width() +
-            this.userPointerBackgroundPaddingX * 2,
+            (userNameNode.width() + this.userPointerBackgroundPaddingX * 2) /
+            stage.scaleX(),
           height:
-            userPointNodeText[0]?.height() +
-            this.userPointerBackgroundPaddingY * 2,
-        });
-        userPointNode[0]?.setAttrs({
-          y:
-            userPointNodeBackground[0]?.y() +
-            userPointNodeBackground[0]?.height() / 2,
+            (userNameNode.height() + this.userPointerBackgroundPaddingY * 2) /
+            stage.scaleX(),
+          cornerRadius: this.userPointerBackgroundCornerRadius / stage.scaleX(),
+          fill: 'rgba(0,0,0,0.2)',
+          strokeWidth: 0,
+          listening: false,
         });
 
-        if (hasChanged) {
-          userPointerNode.setAttrs({
-            x: userPointer.actualPos.x,
-            y: userPointer.actualPos.y,
-            opacity: 1,
-          });
-        }
+        userPointNode.setAttrs({
+          y: userNameBackground.y() + userNameBackground.height() / 2,
+        });
+
+        userPointerNode.add(userPointNode);
+        userPointerNode.add(userNameBackground);
+        userPointerNode.add(userNameNode);
+
+        pointersLayer?.add(userPointerNode);
+
+        continue;
+      }
+
+      const oldPos: Vector2d = {
+        x: userPointer.oldPos.x,
+        y: userPointer.oldPos.y,
+      };
+      const actualPos: Vector2d = {
+        x: userPointer.actualPos.x,
+        y: userPointer.actualPos.y,
+      };
+      const hasChanged = !_.isEqual(actualPos, oldPos);
+
+      // UPDATE TO SCALE
+      const userPointNode = userPointerNode.getChildren(
+        (node) => node.getAttrs().id === 'userPoint'
+      );
+      userPointNode[0]?.setAttrs({
+        radius: this.userPointerCircleRadius / stage.scaleX(),
+        strokeWidth: this.userPointerCircleStrokeWidth / stage.scaleX(),
+      });
+      const userPointNodeText = userPointerNode.getChildren(
+        (node) => node.getAttrs().id === 'userPointName'
+      );
+      userPointNodeText[0]?.setAttrs({
+        x:
+          (this.userPointerSeparation + this.userPointerBackgroundPaddingX) /
+          stage.scaleX(),
+        y: 0 / stage.scaleX(),
+        height: userPointNode[0]?.height(),
+        fontSize: this.userPointerNameFontSize / stage.scaleX(),
+      });
+      const userPointNodeBackground = userPointerNode.getChildren(
+        (node) => node.getAttrs().id === 'userPointRect'
+      );
+      userPointNodeBackground[0]?.setAttrs({
+        x: this.userPointerSeparation / stage.scaleX(),
+        y: -this.userPointerBackgroundPaddingY / stage.scaleX(),
+        cornerRadius: this.userPointerBackgroundCornerRadius / stage.scaleX(),
+        width:
+          userPointNodeText[0]?.width() +
+          this.userPointerBackgroundPaddingX * 2,
+        height:
+          userPointNodeText[0]?.height() +
+          this.userPointerBackgroundPaddingY * 2,
+      });
+      userPointNode[0]?.setAttrs({
+        y:
+          userPointNodeBackground[0]?.y() +
+          userPointNodeBackground[0]?.height() / 2,
+      });
+
+      if (hasChanged) {
+        userPointerNode.setAttrs({
+          x: userPointer.actualPos.x,
+          y: userPointer.actualPos.y,
+          opacity: 1,
+        });
       }
     }
-  }
-
-  pointersRendered(): boolean {
-    return this.doRenderPointers;
-  }
-
-  toggleRenderPointers(): void {
-    this.doRenderPointers = !this.doRenderPointers;
-  }
-
-  setRenderPointers(render: boolean): void {
-    this.doRenderPointers = render;
   }
 
   enable(): void {
