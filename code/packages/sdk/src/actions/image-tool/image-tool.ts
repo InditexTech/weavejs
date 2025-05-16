b3bf2920a6ffef1f51273f0c8c7f77731ee83cf4
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-05-16 09:46:03 +0200

    feat: improve image adding tool

--- code/packages/sdk/src/actions/image-tool/image-tool.ts
+++ code/packages/sdk/src/actions/image-tool/image-tool.ts
@@ -110,13 +110,8 @@ export class WeaveImageToolAction extends WeaveAction {
       if (this.state === IMAGE_TOOL_STATE.ADDING && tempImage) {
         const mousePos = stage.getRelativePointerPosition();
         tempImage.setAttrs({
-          ...this.props,
-          name: undefined,
           x: (mousePos?.x ?? 0) + 2,
           y: (mousePos?.y ?? 0) + 2,
-          fill: '#ccccccff',
-          stroke: '#000000ff',
-          strokeWidth: 1,
         });
 
         const nodeHandler =
@@ -156,10 +151,12 @@ export class WeaveImageToolAction extends WeaveAction {
       if (this.imageId) {
         this.props = {
           ...this.props,
+          imageURL: this.imageURL,
           width: this.preloadImgs[this.imageId].width,
           height: this.preloadImgs[this.imageId].height,
         };
       }
+
       this.addImageNode();
     };
     this.preloadImgs[this.imageId].onerror = () => {
@@ -185,20 +182,25 @@ export class WeaveImageToolAction extends WeaveAction {
     if (this.imageId) {
       const mousePos = stage.getRelativePointerPosition();
 
-      const nodeHandler =
-        this.instance.getNodeHandler<WeaveRectangleNode>('rectangle');
+      const nodeHandler = this.instance.getNodeHandler<WeaveImageNode>('image');
 
       this.tempImageId = uuidv4();
 
+      const aspectRatio =
+        this.preloadImgs[this.imageId].width /
+        this.preloadImgs[this.imageId].height;
+
       const node = nodeHandler.create(this.tempImageId, {
-        ...this.props,
         x: (mousePos?.x ?? 0) + 5,
         y: (mousePos?.y ?? 0) + 5,
-        width: this.preloadImgs[this.imageId].width,
-        height: this.preloadImgs[this.imageId].height,
-        fill: '#ccccccff',
+        width: 100 * aspectRatio,
+        height: 100,
+        opacity: 1,
+        imageURL: this.imageURL,
         stroke: '#000000ff',
-        strokeWidth: 1,
+        strokeWidth: 0,
+        strokeScaleEnabled: false,
+        listening: false,
       });
 
       this.instance.addNode(node, this.container?.getAttrs().id);
@@ -288,31 +290,42 @@ export class WeaveImageToolAction extends WeaveAction {
     return { finishUploadCallback: this.loadImage.bind(this) };
   }
 
-  onPropsChange(): void {
-    const stage = this.instance?.getStage();
-    if (stage) {
-      const tempImage = this.instance
-        .getStage()
-        .findOne(`#${this.tempImageId}`);
+  onPropsChange = undefined;
+  // onPropsChange(): void {
+  //   const stage = this.instance?.getStage();
+  //   if (stage) {
+  //     const tempImage = this.instance
+  //       .getStage()
+  //       .findOne(`#${this.tempImageId}`) as Konva.Image;
 
-      if (tempImage) {
-        tempImage.setAttrs({
-          ...this.props,
-          name: undefined,
-          fill: '#ccccccff',
-          stroke: '#000000ff',
-          strokeWidth: 1,
-        });
+  //     console.log('UPDATE', {
+  //       imageId: this.imageId,
+  //       tempImage,
+  //       imageURL: this.imageURL,
+  //     });
 
-        const nodeHandler =
-          this.instance.getNodeHandler<WeaveRectangleNode>('rectangle');
+  //     if (this.imageId && this.imageURL && tempImage) {
+  //       console.log('SET IMAGE URL', this.imageURL);
+  //       const aspectRatio =
+  //         this.preloadImgs[this.imageId].width /
+  //         this.preloadImgs[this.imageId].height;
 
-        this.instance.updateNode(
-          nodeHandler.serialize(tempImage as WeaveElementInstance)
-        );
-      }
-    }
-  }
+  //       tempImage.setAttrs({
+  //         width: 100 * aspectRatio,
+  //         height: 100,
+  //         imageURL: this.imageURL,
+  //       });
+  //       tempImage.image(this.preloadImgs[this.imageId]);
+
+  //       const nodeHandler =
+  //         this.instance.getNodeHandler<WeaveRectangleNode>('rectangle');
+
+  //       this.instance.updateNode(
+  //         nodeHandler.serialize(tempImage as WeaveElementInstance)
+  //       );
+  //     }
+  //   }
+  // }
 
   cleanup(): void {
     const stage = this.instance.getStage();
