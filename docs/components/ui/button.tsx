939dbdfe45b4dd4e981e3c45651c4d8ef8541d38
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-05-19 19:26:44 +0200

    chore: fix docs button color

--- docs/components/ui/button.tsx
+++ docs/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-white hover:bg-accent hover:text-accent-foreground dark:!text-white dark:!bg-black dark:hover:!bg-[#454545] dark:border-input",
+          "border !text-black bg-white hover:bg-accent hover:text-accent-foreground dark:!text-white dark:!bg-black dark:hover:!bg-[#454545] dark:border-input",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
