15ec89a31e923b10b631ae74798c072aa8d13298
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-06-04 16:21:46 +0200

    Update create-app frontend (#345)
    * chore: initial commit
    
    * feat: update create-app frontend

--- code/packages/create-frontend-app/template/+nextjs+websockets/other.d.ts
+++ code/packages/create-frontend-app/template/+nextjs+websockets/other.d.ts
@@ -1,5 +1,1 @@
-// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
-//
-// SPDX-License-Identifier: Apache-2.0
-
-declare module "platform-detect";
+declare module 'platform-detect';
--- code/packages/create-frontend-app/template/+nextjs+websockets/weave.d.ts
+++ code/packages/create-frontend-app/template/+nextjs+websockets/weave.d.ts
@@ -1,10 +1,6 @@
-// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
-//
-// SPDX-License-Identifier: Apache-2.0
+import 'konva/lib/Node';
 
-import "konva/lib/Node";
-
-declare module "konva/lib/Node" {
+declare module 'konva/lib/Node' {
   interface Node {
     getTransformerProperties(): WeaveNodeTransformerProperties;
     resetCrop(): void;
