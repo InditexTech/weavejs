f852ca90102172bb9831fbed0014fde3f7a3ca25
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-06-10 09:51:44 +0200

    Complete Documentation (#363)
    * chore: initial commit
    
    * feat: update documentation wip
    
    * feat: update documentation

--- docs/components/kbd.tsx
+++ docs/components/kbd.tsx
@@ -8,7 +8,7 @@ export const Kbd = ({ keys }: Readonly<KbdProps>) => {
       {keys.map((key) => (
         <div
           key={key}
-          className="bg-white px-[6px] py-[4px] rounded text-sm font-mono text-black"
+          className="bg-[#eeeeee] px-[6px] py-[4px] rounded text-sm font-mono text-black"
         >
           {key}
         </div>
