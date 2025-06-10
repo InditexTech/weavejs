fb2c8a18a0d4374610268e2c40546fcb37426b87
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-06-10 09:46:35 +0200

    feat: update documentation

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -4,11 +4,28 @@ import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import { Mermaid } from "@/components/mdx/mermaid";
 
+type WrapperProps = {
+  children: React.ReactNode;
+};
+
+const Wrapper = ({ children }: Readonly<WrapperProps>) => {
+  return (
+    <div className="p-5 prose-no-margin bg-gray-100/80 border dark:bg-gray-300/20">
+      {children}
+    </div>
+  );
+};
+
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as any)} />,
+    img: (props) => (
+      <Wrapper>
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        <ImageZoom {...(props as any)} />
+      </Wrapper>
+    ),
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     pre: ({ ref: _ref, ...props }) => (
       <CodeBlock {...props}>
