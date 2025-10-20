e76a609db375773fac6848922d10e1fda22394f0
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-10-20 14:43:36 +0200

    [1.x] Handle loading corrupted images on Image node (#737)
    * chore: initial commit
    
    * feat: handle corrupted images

--- code/packages/sdk/src/actions/image-tool/image-tool.ts
+++ code/packages/sdk/src/actions/image-tool/image-tool.ts
@@ -203,6 +203,7 @@ export class WeaveImageToolAction extends WeaveAction {
           'onImageLoadEnd',
           new Error('Error loading image')
         );
+
         this.cancelAction();
       },
     });
