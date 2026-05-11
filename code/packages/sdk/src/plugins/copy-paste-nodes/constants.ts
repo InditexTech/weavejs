dad00a46f476aea68cc24affb19884b056c105d0
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2026-05-11 10:07:24 +0200

    chore: fix avoid pasting a frame onto a frame when pasting nodes

--- code/packages/sdk/src/plugins/copy-paste-nodes/constants.ts
+++ code/packages/sdk/src/plugins/copy-paste-nodes/constants.ts
@@ -2,8 +2,12 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import type { WeaveStateElement } from '@inditextech/weave-types';
+import type Konva from 'konva';
+
 export const WEAVE_COPY_PASTE_NODES_KEY = 'copyPasteNodes';
 export const WEAVE_COPY_PASTE_PASTE_CATCHER_ID = 'weave-paste-catcher';
+
 export const WEAVE_COPY_PASTE_PASTE_MODES = {
   ['INTERNAL']: 'internal',
   ['EXTERNAL']: 'external',
@@ -11,11 +15,22 @@ export const WEAVE_COPY_PASTE_PASTE_MODES = {
   ['CLIPBOARD_API_ERROR']: 'clipboard-api-error',
   ['CLIPBOARD_API_NOT_SUPPORTED']: 'clipboard-api-not-supported',
 } as const;
+
 export const COPY_PASTE_NODES_PLUGIN_STATE = {
   ['IDLE']: 'idle',
   ['PASTING']: 'pasting',
 } as const;
+
 export const WEAVE_COPY_PASTE_CONFIG_DEFAULT = {
+  canPasteOnto: (node: WeaveStateElement, atTarget: Konva.Container) => {
+    const targetType = atTarget.getAttrs().nodeType;
+
+    if (targetType === 'frame' && node.type === 'frame') {
+      return false;
+    }
+
+    return true;
+  },
   paddingOnPaste: {
     enabled: false,
     paddingX: 0,
